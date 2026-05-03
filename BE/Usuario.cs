5e548bca7bffcb59566ffe71c9c7ff7dda3a8348
Maximiliano <maxiblancocoutoune@gmail.com>
2026-05-02 22:45:23 -0300

    Creado Usuario dentro de BE junto a sus atributos para el login

---
+++ BE/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BE
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string PasswordHash { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaUltimoAcceso { get; set; }
+    }
+}
