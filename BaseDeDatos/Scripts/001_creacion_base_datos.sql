77c689e9497df349b387d9daa23f432952144197
Alejandro Ariel Giorgio <alfiariel@gmail.com>
2026-05-27 20:11:27 -0300

    Corrige script de base de datos con PK y FK nombradas

--- BaseDeDatos/Scripts/001_creacion_base_datos.sql
+++ BaseDeDatos/Scripts/001_creacion_base_datos.sql
@@ -8,21 +8,16 @@ IF OBJECT_ID('dbo.Usuario', 'U') IS NULL
 BEGIN
     CREATE TABLE dbo.Usuario
     (
-        Id uniqueidentifier NOT NULL PRIMARY KEY,
+        Id uniqueidentifier NOT NULL,
         NombreUsuario nvarchar(100) NOT NULL,
         NombreCompleto nvarchar(150) NULL,
         PasswordHash nvarchar(255) NOT NULL,
         Activo bit NOT NULL CONSTRAINT DF_Usuario_Activo DEFAULT (1),
         DebeCambiarPassword bit NOT NULL CONSTRAINT DF_Usuario_DebeCambiarPassword DEFAULT (0),
         FechaCreacion datetime2(7) NOT NULL CONSTRAINT DF_Usuario_FechaCreacion DEFAULT (SYSDATETIME()),
-        FechaUltimoAcceso datetime2(7) NULL
+        FechaUltimoAcceso datetime2(7) NULL,
+        CONSTRAINT PK_Usuario PRIMARY KEY (Id)
     )
-END
-GO
-
-IF COL_LENGTH('dbo.Usuario', 'PasswordSalt') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.Usuario DROP COLUMN PasswordSalt
 END
 GO
 
@@ -34,9 +29,15 @@ GO
 
 IF COL_LENGTH('dbo.Usuario', 'DebeCambiarPassword') IS NULL
 BEGIN
-    ALTER TABLE dbo.Usuario 
-    ADD DebeCambiarPassword bit NOT NULL 
+    ALTER TABLE dbo.Usuario
+    ADD DebeCambiarPassword bit NOT NULL
         CONSTRAINT DF_Usuario_DebeCambiarPassword DEFAULT (0)
+END
+GO
+
+IF COL_LENGTH('dbo.Usuario', 'PasswordSalt') IS NOT NULL
+BEGIN
+    ALTER TABLE dbo.Usuario DROP COLUMN PasswordSalt
 END
 GO
 
@@ -44,14 +45,15 @@ IF OBJECT_ID('dbo.Bitacora', 'U') IS NULL
 BEGIN
     CREATE TABLE dbo.Bitacora
     (
-        Id uniqueidentifier NOT NULL PRIMARY KEY,
+        Id uniqueidentifier NOT NULL,
         Fecha datetime2(7) NOT NULL CONSTRAINT DF_Bitacora_Fecha DEFAULT (SYSDATETIME()),
         UsuarioId uniqueidentifier NULL,
         Usuario nvarchar(100) NULL,
         Modulo nvarchar(100) NOT NULL,
         Accion nvarchar(100) NOT NULL,
         Descripcion nvarchar(500) NULL,
-        Tipo nvarchar(50) NOT NULL
+        Tipo nvarchar(50) NOT NULL,
+        CONSTRAINT PK_Bitacora PRIMARY KEY (Id)
     )
 END
 GO
@@ -64,7 +66,44 @@ GO
 
 IF COL_LENGTH('dbo.Bitacora', 'Modulo') IS NULL
 BEGIN
-    ALTER TABLE dbo.Bitacora ADD Modulo nvarchar(100) NOT NULL CONSTRAINT DF_Bitacora_Modulo DEFAULT ('General')
+    ALTER TABLE dbo.Bitacora
+    ADD Modulo nvarchar(100) NOT NULL
+        CONSTRAINT DF_Bitacora_Modulo DEFAULT ('General')
+END
+GO
+
+DECLARE @pkUsuarioActual sysname;
+
+SELECT @pkUsuarioActual = kc.name
+FROM sys.key_constraints kc
+WHERE kc.parent_object_id = OBJECT_ID('dbo.Usuario')
+  AND kc.[type] = 'PK';
+
+IF @pkUsuarioActual IS NOT NULL AND @pkUsuarioActual <> 'PK_Usuario'
+BEGIN
+    EXEC sp_rename @pkUsuarioActual, 'PK_Usuario';
+END
+GO
+
+DECLARE @pkBitacoraActual sysname;
+
+SELECT @pkBitacoraActual = kc.name
+FROM sys.key_constraints kc
+WHERE kc.parent_object_id = OBJECT_ID('dbo.Bitacora')
+  AND kc.[type] = 'PK';
+
+IF @pkBitacoraActual IS NOT NULL AND @pkBitacoraActual <> 'PK_Bitacora'
+BEGIN
+    EXEC sp_rename @pkBitacoraActual, 'PK_Bitacora';
+END
+GO
+
+IF OBJECT_ID('dbo.FK_Bitacora_Usuario', 'F') IS NULL
+BEGIN
+    ALTER TABLE dbo.Bitacora
+    ADD CONSTRAINT FK_Bitacora_Usuario
+    FOREIGN KEY (UsuarioId)
+    REFERENCES dbo.Usuario(Id)
 END
 GO
 
@@ -106,36 +145,34 @@ GO
 
 IF NOT EXISTS (SELECT 1 FROM dbo.Usuario WHERE NombreUsuario = 'admin')
 BEGIN
-    INSERT INTO dbo.Usuario 
+    INSERT INTO dbo.Usuario
     (
-        Id, 
-        NombreUsuario, 
-        NombreCompleto, 
-        PasswordHash, 
+        Id,
+        NombreUsuario,
+        NombreCompleto,
+        PasswordHash,
         Activo,
         DebeCambiarPassword,
-        FechaCreacion, 
+        FechaCreacion,
         FechaUltimoAcceso
     )
-    VALUES 
+    VALUES
     (
-        NEWID(), 
-        'admin', 
-        'Administrador del Sistema', 
-        'A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ=', 
+        NEWID(),
+        'admin',
+        'Administrador',
+        'A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ=',
         1,
         0,
-        SYSDATETIME(), 
+        SYSDATETIME(),
         NULL
     )
 END
 ELSE
 BEGIN
     UPDATE dbo.Usuario
-    SET NombreCompleto = 'Administrador del Sistema',
-        PasswordHash = 'A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ=',
-        Activo = 1,
-        DebeCambiarPassword = 0
+    SET PasswordHash = 'A6xnQhbz4Vx2HuGl4lXwZ5U2I8iziLRFnhP5eNfIRvQ=',
+        Activo = 1
     WHERE NombreUsuario = 'admin'
 END
 GO
